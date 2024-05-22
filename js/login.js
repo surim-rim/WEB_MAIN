275595e64c4ed0489516fa14c744f00410244213
admin <happy8sr@naver.com>
2024-05-22 15:28:36 +0900

    10주차 완료

--- js/login.js
+++ js/login.js
@@ -1,4 +1,7 @@
-//이거 나중에 다시 손으로 써서 정리 다시하기
+//여기 나중에 다시 손으로 써서 정리 다시하기
+
+
+
 const check_xss = (input) => {
     // DOMPurify 라이브러리 로드 (CDN 사용)
     const DOMPurify = window.DOMPurify;
@@ -15,11 +18,17 @@ const check_xss = (input) => {
     };
 
 
+
 const check_input = () => {
+    //전역 변수 추가, 맨 위 위치
+    const idsave_check = document.getElementById('idSaveCheck');
+
     const loginForm = document.getElementById('login_form');
     const loginBtn = document.getElementById('login_btn');
     const emailInput = document.getElementById('form3Example3');
     const passwordInput = document.getElementById('form3Example4');
+    
+
     const c = '아이디, 패스워드를 체크합니다';
     alert(c);
     const emailValue = emailInput.value.trim();
@@ -76,6 +85,53 @@ if (!sanitizedPassword) {
 return false;
 }
 
+
+
+function setCookie(name, value, expiredays){
+    var date = new Date();
+    date.setDate(date.getDate() + expiredays);
+    document.cookie = escape(name) + "=" + escape(value) + "; expires=" + date.toUTCString() + "; path=/" + ";SameSite=None; Secure";
+}
+
+function getCookie(name){
+    var cookie = document.cookie;
+    console.log("쿠키를 요청합니다.");
+    if (cookie != ""){
+        var cookie_array = cookie.split("; ");
+        for(var index in cookie_array){
+            var cookie_name = cookie_array[index].split("=");
+
+            if(cookie_name[0] == "id"){
+                return cookie_name[1];
+            }
+        }
+    }
+    return ;
+}
+
+//검사 마무리 단계 쿠키 저장, 최하단 submit 이전
+if(idsave_check.checked == true){
+    alert("쿠키를 저장합니다.", emailValue);
+    setCookie("id", emailValue, 1); //1일 저장
+    alert("쿠키 값 :" + emailValue);
+}
+else
+{   //아이디 체크x
+    setCookie("id", emailValue.value, 0); //날짜를 0 - 쿠키 삭제
+}
+
+function init(){
+    const emailInput = document.getElementById('typeEmailX');
+    const idsave_check = document.getElementById('idSaveCheck');
+    let get_id = getCookie("id");
+
+    if(get_id){
+        emailInput.value = get_id;
+        idsave_check.checked = true;
+    }
+}
+
+
     console.log('이메일:', emailValue);
     console.log('비밀번호:', passwordValue);
     loginForm.submit();
@@ -106,3 +162,4 @@ const checkConsecutiveNumbers = (str, length) => {
 
 
     document.getElementById("login_btn").addEventListener('click', check_input);
+    
